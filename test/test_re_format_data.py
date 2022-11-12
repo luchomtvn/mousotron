1fbf5424e1251f51ac432e845ae8a8a48068e22b
lucmtv <lucianomanto@gmail.com>
2022-11-12 17:56:12 -0300

    test cents and date

--- test/test_re_format_data.py
+++ test/test_re_format_data.py
@@ -4,6 +4,7 @@ import pytest
 import unittest
 import warnings
 import logging
+from datetime import datetime
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -18,11 +19,14 @@ class TestCommon(unittest.TestCase):
         self.assertEqual(get_seconds('1 days 0 hours 0 min 50 sec'), 86450)
         self.assertEqual(get_seconds('1 days 1 hours 0 min 0 sec'), 90000)
     def test_get_cents(self):
-        self.assertTrue(True)
-        warnings.warn("test not defined")
+        self.assertEqual(get_cents('0 km 00 m 0 cm'), 0)
+        self.assertEqual(get_cents('0000 km 0000 m 0000 cm'), 0)
+        self.assertEqual(get_cents('0000 km 0000 m 1 cm'), 1)
+        self.assertEqual(get_cents('0 km 1 m 0 cm'), 100)
+        self.assertEqual(get_cents('1 km 1 m 0 cm'), 100100)
+        self.assertEqual(get_cents('1 km 53 m 94 cm'), 105394)
     def test_format_date(self):
-        self.assertTrue(True)
-        warnings.warn("test not defined")
+        self.assertEqual(format_date('2020 abr. 09 01:34:37'), datetime(2020, 4, 9, 1, 34, 37))
     def test_format_row(self):
         self.assertTrue(True)
         warnings.warn("test not defined")
